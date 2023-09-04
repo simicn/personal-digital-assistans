79152e875d63121ba66a65a07e939cf36e7b0e84
Nina <nikolina__simic__@hotmail.com>
2023-09-04 17:26:09 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css';
+import ProfileCart from './ProfileCart';
+import AlexaImage from './images/alexa.png';
+import CortanaImge from './images/cortana.png';
+import SiriImage from './images/siri.png';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section class="hero is-primary">
+        <div class="hero-body">
+          <p class="title">
+            Pesonal digital assistans
+          </p>
+          
+        </div>
+      </section>
+
+      <div ></div>
+      <div className='container'>
+        <section className='section'>
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCart title="Alexa" handle="@hshsh" image={AlexaImage}
+              description="Alexa was created by Amazon."
+              ></ProfileCart>
+            </div>
+            <div className="column is-3">
+              <ProfileCart title="Siri" handle="@hshsh" image={SiriImage}
+              description="Siri was created by Microsoft."></ProfileCart>
+
+            </div>
+            <div className="column is-3">
+              <ProfileCart title="Cortana" handle="@hshsh" image={CortanaImge}
+              description="Cortana was created by Apple."></ProfileCart>
+
+            </div>
+          </div>
+        </section>
+      </div>
+
     </div>
   );
 }
